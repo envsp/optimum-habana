f0404c19fc5f3c4777902ba92c75265a42d62b91
Evsikov, Pavel <pavel.evsikov@intel.com>
2024-11-07 15:55:39 -0800

    [temp] fix 8.3

--- optimum/habana/transformers/generation/utils.py
+++ optimum/habana/transformers/generation/utils.py
@@ -2531,7 +2531,7 @@ class GaudiGenerationMixin(GenerationMixin):
                 is_mqa_model = self.config.model_type == "gpt_bigcode" and self.config.multi_query
                 model_kwargs["mqa_model"] = is_mqa_model
                 if is_mqa_model:
-                    do_padding = "input_ids" in model_inputs and outputs.past_key_values[0].shape[1] == model_inputs["input_ids"].shape[1]
+                    do_padding = "input_ids" in model_inputs and outputs.past_key_values[0][0].shape[1] == model_inputs["input_ids"].shape[1]
                 else:
                     key_to_check = (
                         "input_ids"
